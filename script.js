b3a7c808df90ac0c2871ed335a6695c9152efa3f
Matt <matthiasvstephens@gmail.com>
2021-09-21 10:25:00 +0200

    add copy function + copied message

--- script.js
+++ script.js
@@ -1,9 +1,11 @@
 // Assignment Code
+
+//generate btn
 var generateBtn = document.querySelector("#generate");
 
 // Working function
 generateBtn.addEventListener('click', function generatePassword(){
-  //empty string for pw
+  //empty string for pw to go in
   var generatedPassword = '';
   //character vars
   var usedCharacters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
@@ -15,6 +17,12 @@ generateBtn.addEventListener('click', function generatePassword(){
   //if charactercount not meet requirements, alert and terminate func
   if (characterCount < 8 || characterCount >= 128) {
     alert("Try again. Type in a value between 8 and 128")
+    return
+  }
+  // NOTE: I got regex help for this part from https://stackoverflow.com/questions/23476532/check-if-string-contains-only-letters-in-javascript
+  // if contains letters
+  if (/^[a-zA-Z]+$/.test(characterCount)) {
+    alert("Please enter a number.")
     return
   }
   //characterCount to num from string
@@ -37,16 +45,28 @@ generateBtn.addEventListener('click', function generatePassword(){
   if (specialsYOrN) {
     usedCharacters = usedCharacters.concat(specials)
   }
+  //using charAt since didn't make it array
   console.log(usedCharacters)
   for(let i = 0; i < charsWanted; i++){
     generatedPassword += usedCharacters.charAt(Math.floor(Math.random() * usedCharacters.length))
   }
   //console.log(generatedPassword)
+
   document.querySelector("#password").value = generatedPassword;
   return generatedPassword
 });
 
-/* Don't need these
+//copy the password in one click
+document.getElementById('password').addEventListener('click', function(){
+  document.getElementById('password').select();
+  //i know execcommand is deprecated but idk how else to do this tbh
+  document.execCommand('copy');
+  document.querySelector('h3').style.opacity = "100"
+  setTimeout(function(){document.querySelector('h3').style.opacity = "0";}, 1000);
+})
+
+
+/* don't need these tbh
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
